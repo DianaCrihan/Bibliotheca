3d19a72a4c769999ef4ec0ff792c038058fb6cd7
DianaCrihan <crihan.diana@gmail.com>
2025-02-08 14:28:23 +0200

    Initial Commit

---
+++ Bibliotheca/BibliothecaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibliothecaApp.swift
+//  Bibliotheca
+//
+//  Created by Diana Crihan on 08.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct BibliothecaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
